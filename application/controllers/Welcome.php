596cd7d715c8ea8a03c56d16d30a816967e4616e
mamat08nurahmat <mamat08nurahmat@gmail.com>
2019-02-26 11:17:38 +0700

    database config

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+	//	$this->load->view('welcome_message');
+	print_r($this->db->query("SELECT TOP 5 * FROM Agency")->result());
+	
 	}
 }
